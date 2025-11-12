29063a418b263266bfd417c2897d844c7609c5e6
Luis Angel Villanueva <luis.angel.villanueva@outlook.com>
2025-11-11 20:55:22 -0800

    Added experimental script to refresh all users addons.

--- README.md
+++ README.md
@@ -1,28 +1,42 @@
 # AIOStreams Management Scripts (unofficial)
+
 ## Reasoning Behind This
+
 I got annoyed of juggling account configurations for my family, so I decided to make some scripts to do it for me!
 
 # Create User
+
 ## Purpose
+
 Creates a new user account with a random password using the config at `AIOSTREAMS_CONFIG_PATH` and adds it to the accounts.json at `ACCOUNTS_JSON_PATH`
+
 ## Command
+
 ```
 python create_user.py
 ```
+
 ## Example Usage
+
 ```
 python create_user.py
 User created successfully: {'uuid': 'ce4ce4cd-f733-4dae-be68-601e92eb02da', 'password': 'GRAcmTLjGD38HTJJM32zjdTkUFIH15uw5g3PciflSmgmKULVynsEoWn4cUdS0kI-uqK67pTilbjt0mm0jpS98oRwACo2XyI27yuxOKcACwBqlDUHt_Wb1jFQhygs8n_Adz1zCZlaMOccMcxH6LBuOSzxGMzbCOULu19CT7PH71U', 'manifest': 'https://<your-domain>/stremio/ce4ce4cd-f733-4dae-be68-601e92eb02da/eyJpdiI6ImVPN0pFaWpKRlJCMmduZDh2OFdCNVE9PSIsImVuY3J5cHRlZCI6ImVVNGkyQVJvY1pjRWgvU3ZyR3VLazlsWTByZTF5UU0vRDlTRStFRXQxTFVoRnRPdXZINjY2WUlhWFF1am9sT2QwSGxGRlVDcjZyejN3Y3VoRnAzUW9Md2V4dWttM0hJTmZuekF2Wit3V0JvWUJDdndqQ0ZGaEJ5cGxVQ1J5d21TWnZQZHJGc2dGcVNubTFDNFU4RVNHejFOMjV3dllxWDk3K0FVOVhwTHZwazJFZUtmOFliODlTOXo2MXREdCtlV3lxMWlwLzF6SHg3ekswcEdQQWZXeXk0ZU14SVA0UFkrdkV5QXdvaG5seDQ9IiwidHlwZSI6ImFpb0VuY3J5cHQifQ/manifest.json'}
 ```
 
 # Delete User
+
 ## Purpose
+
 Deletes a user account using the password located in the accounts.json at `ACCOUNTS_JSON_PATH` and also removes it from there
+
 ## Command
+
 ```
 python delete_user.py
 ```
+
 ## Example Usage
+
 ```
 Enter the UUID of the user to delete: ce4ce4cd-f733-4dae-be68-601e92eb02da
 Processing account: ce4ce4cd-f733-4dae-be68-601e92eb02da
@@ -30,15 +44,40 @@ Successfully deleted user: ce4ce4cd-f733-4dae-be68-601e92eb02da
 ```
 
 # Update All Users
+
 ## Purpose
+
 Updates all user accounts located in the accounts.json at `ACCOUNTS_JSON_PATH` with the configuration at `AIOSTREAMS_CONFIG_PATH`
+
 ## Command
+
 ```
 python update_all_users.py
 ```
+
 ## Example Usage
+
 ```
 python update_all_users.py
 Processing account: ce4ce4cd-f733-4dae-be68-601e92eb02da
 Successfully updated account: ce4ce4cd-f733-4dae-be68-601e92eb02da
 ```
+
+# Refresh All Users Addons (Experimental)
+
+## Purpose
+
+It updates the addons for all the users located in the stremio_accounts.json at `STREMIO_ACCOUNTS_JSON_PATH`. NOTE: It comes with the added benefit of removing parts of Cinemeta for better compatibility with [AIOMetadata](https://github.com/cedya77/aiometadata). Inspired from [Cinebye](https://github.com/0xConstant1/stremio-addon-manager).
+
+## Example Usage
+
+```
+python refresh_all_users_addons.py
+Processing account: email1@example.com
+Successfully updated addons: email1@example.com
+```
+
+## Note
+
+- From experimenting, it sometimes throws a connection refused error. Just try again later.
+- If it fails anywhere, the program should exit, but there's no guarantee your configuration won't break. If something does break, then running [this](https://web.archive.org/web/20250220150116id_/https://pastebin.com/raw/VUPfMnGk) in your browser console on [Stremio Web](https://web.stremio.com) should completely reset your Stremio account addons as if you just created a new account.
---
+++ refresh_all_users_addons.py
@@ -0,0 +1,204 @@
+import json
+import os
+import time
+import urllib.request
+from utils import load_dotenv
+
+# Usage: python refresh_all_users_addons.py
+# This script updates all user accounts
+
+
+def get_auth_key(email, password, login_url):
+    """Fetches the auth key for the given email and password. Returns the auth key or None if there was an error."""
+
+    data = json.dumps(
+        {
+            "authKey": None,
+            "email": email,
+            "password": password,
+        }
+    ).encode("utf-8")
+
+    req = urllib.request.Request(
+        login_url,
+        data=data,
+        method="POST",
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
+
+    try:
+        resp = urllib.request.urlopen(req, timeout=10)
+        response = resp.read().decode("utf-8")
+        response = json.loads(response)
+        auth_key = response.get("result", {}).get("authKey", None)
+        return auth_key
+    except urllib.error.HTTPError:
+        return None
+    except urllib.error.URLError:
+        print("Connection refused.")
+        exit(1)
+
+
+def get_addons(auth_key, base_url):
+    """Fetches the addons for the given auth key. Returns a list of addons or None if there was an error."""
+
+    url = f"{base_url}/addonCollectionGet"
+    data = json.dumps({"authKey": auth_key, "type": "AddonCollectionGet"}).encode(
+        "utf-8"
+    )
+
+    req = urllib.request.Request(
+        url,
+        data=data,
+        method="POST",
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
+
+    try:
+        resp = urllib.request.urlopen(req, timeout=10)
+        response = resp.read().decode("utf-8")
+        addons = json.loads(response).get("result", {}).get("addons", None)
+        return addons
+    except urllib.error.HTTPError:
+        return None
+    except urllib.error.URLError:
+        print("Connection refused.")
+        exit(1)
+
+
+def update_addons(addons):
+    """Updates the addon manifest to the latest version. Returns the updated addons."""
+
+    updated_addons = []
+
+    for addon in addons:
+        manifestUrl = addon.get("transportUrl", None)
+        if manifestUrl is None:
+            print(f"Manifest URL not found: {addon.get('id', 'unknown')}")
+            exit(1)
+
+        req = urllib.request.Request(
+            manifestUrl,
+            method="GET",
+            headers={
+                "Content-Type": "application/json",
+            },
+        )
+
+        try:
+            resp = urllib.request.urlopen(req, timeout=10)
+            response = resp.read().decode("utf-8")
+            manifest = json.loads(response)
+
+            updated_addon = {
+                **addon,
+                "manifest": manifest,
+            }
+
+            if addon["manifest"]["name"] == "Cinemeta":
+                catalogs = updated_addon.get("manifest", {}).get("catalogs", [])
+                new_catalogs = []
+                for catalog in catalogs:
+                    if catalog.get("id") in ["last-videos", "calendar-videos"]:
+                        new_catalogs.append(catalog)
+                updated_addon["manifest"]["catalogs"] = new_catalogs
+                updated_addon["manifest"]["resources"] = ["catalog", "addon_catalog"]
+
+            updated_addons.append(updated_addon)
+        except urllib.error.HTTPError:
+            continue
+        except urllib.error.URLError:
+            print("Connection refused.")
+            exit(1)
+
+    return updated_addons if updated_addons else None
+
+
+def set_addons(auth_key, addons, base_url):
+    """Sets the addons for the given auth key. Returns the response from the server or None if there was an error."""
+
+    url = f"{base_url}/addonCollectionSet"
+    data = json.dumps(
+        {
+            "authKey": auth_key,
+            "type": "AddonCollectionSet",
+            "addons": addons,
+        }
+    ).encode("utf-8")
+
+    req = urllib.request.Request(
+        url,
+        data=data,
+        method="POST",
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
+
+    try:
+        resp = urllib.request.urlopen(req, timeout=10)
+        response = resp.read().decode("utf-8")
+        success = json.loads(response).get("result", {}).get("success", False)
+        return success
+    except urllib.error.HTTPError:
+        return False
+    except urllib.error.URLError:
+        print("Connection refused.")
+        exit(1)
+
+
+def main():
+    load_dotenv()
+
+    BASE_STREMIO_API_URL = os.getenv("BASE_STREMIO_API_URL")
+    STREMIO_ACCOUNTS_JSON_PATH = os.getenv("STREMIO_ACCOUNTS_JSON_PATH")
+
+    if not all([BASE_STREMIO_API_URL, STREMIO_ACCOUNTS_JSON_PATH]):
+        print("One or more environment variables are not set.")
+        return
+
+    if BASE_STREMIO_API_URL.endswith("/"):
+        BASE_STREMIO_API_URL = BASE_STREMIO_API_URL[:-1]
+
+    LOGIN_URL = f"{BASE_STREMIO_API_URL}/login"
+
+    with open(STREMIO_ACCOUNTS_JSON_PATH, "r", encoding="utf-8") as f:
+        accounts = json.load(f)["accounts"]
+
+    for account in accounts:
+        email = account["email"]
+        password = account["password"]
+
+        print(f"Processing account: {email}")
+
+        auth_key = get_auth_key(email, password, LOGIN_URL)
+        if auth_key is None:
+            print(f"Failed to retrieve auth key: {email}")
+            exit(1)
+
+        addons = get_addons(auth_key, BASE_STREMIO_API_URL)
+        if addons is None:
+            print(f"Failed to retrieve addons: {email}")
+            exit(1)
+
+        updated_addons = update_addons(addons)
+        if updated_addons is None:
+            print(f"Failed to update addons: {email}")
+            exit(1)
+
+        success = set_addons(auth_key, updated_addons, BASE_STREMIO_API_URL)
+        if not success:
+            print(f"Failed to update addons: {email}")
+            exit(1)
+
+        print(f"Successfully updated addons: {email}")
+
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
